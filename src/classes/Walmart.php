c0ca5d0b6293ba8dda146b368b24c4bddd7ef21e
Denis Dyachenko <DeniskasViskas4@gmail.com>
2020-10-19 23:17:37 +0300

    add new method
    itemDelete

--- src/classes/Walmart.php
+++ src/classes/Walmart.php
@@ -24,7 +24,6 @@
 			$this->base_url = $base_url;
 			$this->channel_type = $channel_type;
 		}
-
 		/**
 		 * @return mixed
 		 * token time live 900
@@ -57,14 +56,36 @@
 			}
 			return $response;
 		}
+		/**
+		 * @param array $itemData
+		 * @param $xml
+		 * @return SimpleXMLElement
+		 */
+		private function array_to_xml($itemData, &$xml) {
+			foreach($itemData as $key => $value) {
+				if(is_array($value)) {
+					if(!is_numeric($key)){
+						$sub_node = $xml->addChild("$key");
+						$this->array_to_xml($value, $sub_node);
+					}
+					else{
+						$this->array_to_xml($value, $xml);
+					}
+				}
+				else {
+					$xml->addChild("$key","$value");
+				}
+			}
+			return $xml->asXML();
+		}
 
 		/**
-		 * @param null $feedId
+		 * @param string|null $feedId
 		 * @param int $offset
 		 * @param int $limit
 		 * @return mixed
 		 */
-		public function getFeedList($feedId = null, $offset = 0, $limit = 50) {
+		public function getFeedList($feedId = null, $offset = 0, $limit = 20) {
 			$curl = curl_init();
 			$get_params = [
 				'offset' => $offset,
@@ -94,26 +115,26 @@
 				],
 			]);
 			$response = curl_exec($curl);
-			return json_decode($response);
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
 		}
 
 		/**
-		 * @param null $sku
+		 * @param string $feedId
 		 * @param int $offset
 		 * @param int $limit
 		 * @return mixed
 		 */
-		public function getItems($sku = null, $offset = 0, $limit = 50) {
+		public function getFeedDetails($feedId, $offset = 0, $limit = 20) {
 			$curl = curl_init();
 			$get_params = [
 				'offset' => $offset,
 				'limit' => $limit,
-				'nextCursor' => '*'
+				'includeDetails'=>'true'
 			];
-			if (!is_null($sku)) {
-				$get_params['sku'] = $sku;
-			}
-			$url = $this->base_url . '/items?' . http_build_query($get_params);
+			$url = $this->base_url . '/feeds/'.$feedId."?" . http_build_query($get_params);
 			curl_setopt_array($curl, [
 				CURLOPT_URL => $url,
 				CURLOPT_RETURNTRANSFER => true,
@@ -134,11 +155,99 @@
 				],
 			]);
 			$response = curl_exec($curl);
-			return json_decode($response);
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
 		}
 
 		/**
-		 * @param $sku
+		 * @param string|null $sku
+		 * @param null $nextCursor
+		 * @param int $offset
+		 * @param int $limit
+		 * @return mixed
+		 */
+		public function getItems($sku = null,$nextCursor=null, $offset = 0, $limit = 20) {
+			$curl = curl_init();
+			$get_params = [
+				'offset' => $offset,
+				'limit' => $limit,
+			];
+			if (!is_null($sku)) {
+				$get_params['sku'] = $sku;
+			}
+			if (!is_null($nextCursor)){
+				$get_params = [];
+				$get_params['nextCursor'] =$nextCursor;
+			}
+			$url = $this->base_url . '/items?' . http_build_query($get_params);
+			curl_setopt_array($curl, [
+				CURLOPT_URL => $url,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "GET",
+				CURLOPT_SSL_VERIFYHOST => false,
+				CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_HTTPHEADER => [
+					"Authorization: Basic " . base64_encode($this->client_id . ':' . $this->client_secret),
+					"WM_SVC.NAME: Walmart Marketplace",
+					"WM_QOS.CORRELATION_ID:" . uniqid(),
+					"WM_SEC.ACCESS_TOKEN:" . $this->getToken(),
+					"Content-Type: application/json",
+					"Accept: application/json"
+				],
+			]);
+			$response = curl_exec($curl);
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
+		}
+
+		/**
+		 * @param $itemArray
+		 * @return bool|mixed|string
+		 */
+		public function bulkAddItem($itemArray){
+			$xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"UTF-8\"?><MPItemFeed xmlns=\"http://walmart.com/\"></MPItemFeed>");
+			$curl = curl_init();
+			$get_params = [
+				'feedType' => 'item'
+			];
+			$url = $this->base_url . '/feeds?' . http_build_query($get_params);
+			curl_setopt_array($curl, [
+				CURLOPT_URL => $url,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "POST",
+				CURLOPT_POSTFIELDS => $this->array_to_xml($itemArray,$xml),
+				CURLOPT_SSL_VERIFYHOST => false,
+				CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_HTTPHEADER => [
+					"Authorization: Basic " . base64_encode($this->client_id . ':' . $this->client_secret),
+					"WM_SVC.NAME: Walmart Marketplace",
+					"WM_QOS.CORRELATION_ID:" . uniqid(),
+					"WM_SEC.ACCESS_TOKEN:" . $this->getToken(),
+					"Content-Type: application/xml",
+					"Accept: application/json"
+				],
+			]);
+			$response = curl_exec($curl);
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
+		}
+
+		/**
+		 * @param string|null $sku
 		 * @param int $shipNode
 		 * @return mixed
 		 */
@@ -169,11 +278,14 @@
 				],
 			]);
 			$response = curl_exec($curl);
-			return json_decode($response);
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
 		}
 
 		/**
-		 * @param string $sku
+		 * @param string|null $sku
 		 * @param int $qty
 		 * @param int $shipNode
 		 * @return mixed
@@ -213,7 +325,40 @@
 				],
 			]);
 			$response = curl_exec($curl);
-			return json_decode($response);
-
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
 		}
+
+		public function deleteItem($sku){
+			$curl = curl_init();
+			$url = $this->base_url . '/items/'.$sku;
+			curl_setopt_array($curl, [
+				CURLOPT_URL => $url,
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 0,
+				CURLOPT_FOLLOWLOCATION => true,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => "DELETE",
+				CURLOPT_SSL_VERIFYHOST => false,
+				CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_HTTPHEADER => [
+					"Authorization: Basic " . base64_encode($this->client_id . ':' . $this->client_secret),
+					"WM_SVC.NAME: Walmart Marketplace",
+					"WM_QOS.CORRELATION_ID:" . uniqid(),
+					"WM_SEC.ACCESS_TOKEN:" . $this->getToken(),
+					"Content-Type: application/json",
+					"Accept: application/json"
+				],
+			]);
+			$response = curl_exec($curl);
+			if (!is_null(json_decode($response))){
+				return json_decode($response);
+			}
+			return $response;
+		}
+
+
 	}
